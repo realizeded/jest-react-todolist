5b0971912378384fa61bd92e4a937a4b6be7b278
lianjun <lianjun>
2021-01-23 10:59:23 +0800

    intergration

---
+++ src/pages/TodoList/__tests__/intergration/intergration.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Enzyme,{mount}from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TodoList from '../../TodoList';
+Enzyme.configure({ adapter: new Adapter() });
+it(`
+1.输入框输入内容
+2.点击回车
+3.列表项展示
+`,()=>{
+    const wrapper = mount(<TodoList/>);
+    const listDate = [
+        {state:"input",value:""}
+    ]
+    wrapper.setState({undoList:listDate});
+    const inputEle = wrapper.find('[data-test="input"]');
+    const content = 'content';
+    inputEle.at(0).simulate('change',{
+        target:{
+            value:content
+        }
+    });
+    inputEle.at(0).simulate('keyup',{
+        keyCode:13
+    });
+    const listItem = wrapper.find('[data-test="listItem"]');
+    expect(listItem.length).toBe(2);
+});
